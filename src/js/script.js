36b0eeed24a40bebf4a356cea45173bdd7dbf19a
Sebastian <spaluszkiewicz@gmail.com>
2019-01-15 10:15:19 +0100

    new item description card, slider, store icons some little changes

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() {
+    console.log( "ready!" );
+});
+
+
+$('.slider').glide({
+    autoplay: 20000,
+    arrowsWrapperClass: 'slider-arrows',
+    arrowRightText: '',
+    arrowLeftText: ''
+});
+
